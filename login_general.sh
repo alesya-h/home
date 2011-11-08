16248e4302e875f2b1a574d7705d32ae11b29e39
Ales Guzik <ales.guzik@gmail.com>
2011-11-08 16:20:35 +0300

    changed browser to conkeror

--- login_general.sh
+++ login_general.sh
@@ -3,7 +3,7 @@ export LOGIN_GENERAL_LOADED=true
 
 export EDITOR="emacsclient"
 export ALTERNATE_EDITOR="emacs"
-export BROWSER=firefox
+export BROWSER=conkeror
 export CLICOLOR=1
 
 export PATH="$HOME/.rbenv/bin:$PATH:$HOME/p/my/scripts:$HOME/bin:$HOME/.local/bin"
