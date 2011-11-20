d648fc7b77cfcc9892a251b366935ab3b534ac98
Ales Guzik <ales.guzik@gmail.com>
2011-11-20 12:03:30 +0300

    kidsvt lab1 update

--- bsuir/code/kidsvt/1.lisp
+++ bsuir/code/kidsvt/1.lisp
@@ -1,51 +1,12 @@
-(defvar *scheme*
-  `((f1 (not x3))
-    (f2 (or x5 x6))
-    (f3 (and x1 x2))
-    (f4 (and x4 f2 x7))
-    (f5 (and f1 f4))
-    (f6 (or f3 f5))))
-
-(defun try-scheme (scheme out &optional
-                   (x1 nil)
-                   (x2 nil)
-                   (x3 nil)
-                   (x4 nil)
-                   (x5 nil)
-                   (x6 nil)
-                   (x7 nil)
-                   (x8 nil))
-  (eval `(let ((x1 ,x1)
-               (x2 ,x2)
-               (x3 ,x3)
-               (x4 ,x4)
-               (x5 ,x5)
-               (x6 ,x6)
-               (x7 ,x7)
-               (x8 ,x8))
-           (let* ,scheme
-             ,out
-             ))))
-
-(defmacro use-scheme (sch 
-  `(let ((x1 ,x1)
-         (x2 ,x2)
-         (x3 ,x3)
-         (x4 ,x4)
-         (x5 ,x5)
-         (x6 ,x6)
-         (x7 ,x7)
-         (x8 ,x8))
-     (,sch)))
-
-(defclass circuit-component ()
-  (component-singular
-  component-d
-  number-element
-  number-out-slot
-  number-in-slot
-  check))
-
-(defmethod set-arrays ((sing vector) (d vector) (cc circuit-component))
-  (setf (slot-value cc 'component-singular) sing)
-  (setf (slot-value cc 'component-d) d))
+(defun make-d (a b)
+  (cond
+    ((and (= a 0) (= b 0)) 0)
+    ((and (= a 0) (= b :x)) 0)
+    ((and (= a :x) (= b 0)) 0)
+    ((and (= a :x) (= b :x)) :x)
+    ((and (= a 1) (= b 1)) 1)
+    ((and (= a 1) (= b :x)) 1)
+    ((and (= a :x) (= b 1)) 1)
+    ((and (= a 1) (= b 0)) :d)
+    ((and (= a 0) (= b 1)) :^d)
+    (t (error "wrong input"))))
