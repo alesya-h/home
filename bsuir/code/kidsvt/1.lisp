7dcd75746f93c55262a3f4f162eb171c047f2984
Ales Guzik <ales.guzik@gmail.com>
2011-11-21 20:16:15 +0300

    kidsvt lab upgrade

--- bsuir/code/kidsvt/1.lisp
+++ bsuir/code/kidsvt/1.lisp
@@ -1,12 +1,43 @@
-(defun make-d (a b)
+(defvar *scheme*
+  `((f1 not x3)
+    (f2 or x5 x6)
+    (f3 and x1 x2)
+    (f4 and x4 f2 x7)
+    (f5 and f1 f4)
+    (f6 or f3 f5)))
+
+(defparameter +selected+ 'f3)
+
+(defun get-singular-cube (row)
+  (caadr row
+
+(defmacro fn (&rest args)
+  `(lambda ,@args))
+
+(defun get-deps (elem)
+  (cdadr elem))
+
+(defun make-d-cubes (a b)
   (cond
-    ((and (= a 0) (= b 0)) 0)
-    ((and (= a 0) (= b :x)) 0)
-    ((and (= a :x) (= b 0)) 0)
-    ((and (= a :x) (= b :x)) :x)
-    ((and (= a 1) (= b 1)) 1)
-    ((and (= a 1) (= b :x)) 1)
-    ((and (= a :x) (= b 1)) 1)
-    ((and (= a 1) (= b 0)) :d)
-    ((and (= a 0) (= b 1)) :^d)
+    ((and (eq a  0) (eq b  0))   0)
+    ((and (eq a  0) (eq b :x))   0)
+    ((and (eq a :x) (eq b  0))   0)
+    ((and (eq a :x) (eq b :x))  :x)
+    ((and (eq a  1) (eq b  1))   1)
+    ((and (eq a  1) (eq b :x))   1)
+    ((and (eq a :x) (eq b  1))   1)
+    ((and (eq a  1) (eq b  0))  :d)
+    ((and (eq a  0) (eq b  1)) :^d)
     (t (error "wrong input"))))
+
+(defun intersect-d-cubes (a b)
+  (let ((rez (mapcar (fn (ai bi)
+              (cond
+                ((eq ai bi) ai)
+                ((eq ai :x) bi)
+                ((eq bi :x) ai)
+                (t nil)))
+          a b)))
+    (if (position nil rez)
+        nil
+        rez)))
