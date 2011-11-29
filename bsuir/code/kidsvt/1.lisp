5d453bd84e702d7e3fa0c3d797b637b4985c7c27
Ales Guzik <ales.guzik@gmail.com>
2011-11-29 22:19:38 +0300

    kidsvt lab update

--- bsuir/code/kidsvt/1.lisp
+++ bsuir/code/kidsvt/1.lisp
@@ -1,18 +1,48 @@
-(defvar *scheme*
-  `((f1 not x3)
-    (f2 or x5 x6)
-    (f3 and x1 x2)
-    (f4 and x4 f2 x7)
-    (f5 and f1 f4)
-    (f6 or f3 f5)))
-
-(defparameter +selected+ 'f3)
-
-(defun get-singular-cube (row)
-  (caadr row
-
 (defmacro fn (&rest args)
   `(lambda ,@args))
+
+(defun filter (fn 1st)
+  (let ((ace nil))
+    (dolist (x 1st)
+      (let ((val (funcall fn x)))
+        (if val (push x ace))))
+    (nreverse ace)))
+
+(defparameter *scheme*
+  (list '(:x1 :x2 :x3 :x4 :x5 :x6)
+    '(:f1 (not x3)
+      :f2 (or x5 x6)
+      :f3 (and x1 x2)
+      :f4 (and x4 f2 x7)
+      :f5 (and f1 f4)
+      :f6 (or f3 f5))
+    '(:f6)))
+
+(defun get-names (scheme)
+  (concatenate 'list
+               (car scheme) 
+               (filter #'symbolp (get-defs scheme))))
+
+(defun get-defs (scheme)
+  (cadr scheme))
+(defun get-outs (scheme)
+  (caddr scheme))
+
+(defun make-singular-cube (elt scheme)
+  )
+
+(defun refrences? (who whom scheme)
+  (if (position who (getf (get-defs scheme) whom) t nil)))
+
+(defun elements-just-after (points scheme)
+  (let (all (get-defs scheme)
+  (loop for i in point
+             do (progn
+                  (
+(defparameter +selected+ 'f3)
+
+;(defun get-singular-cube (row)
+;  (caadr row
 
 (defun get-deps (elem)
   (cdadr elem))
