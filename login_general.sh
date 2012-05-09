738b6e3496c7bdece6a6e07f3f7ff86069a16818
Ales Guzik <ales.guzik@gmail.com>
2012-05-09 13:03:24 +0300

    sox fix for shelr audio recording

--- login_general.sh
+++ login_general.sh
@@ -4,5 +4,6 @@ export LOGIN_GENERAL_LOADED=true
 export EDITOR="vim"
 export BROWSER="firefox"
 export CLICOLOR=1
+export AUDIODRIVER="alsa"
 
 export PATH="$HOME/.rbenv/bin:$PATH:$HOME/p/my/scripts:$HOME/p/my/fork/ruby-build/bin:$HOME/bin:$HOME/.local/bin"
