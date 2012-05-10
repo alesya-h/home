8395695322cd05bc8dce3ae57f2efa6be9baceba
Ales Guzik <ales.guzik@gmail.com>
2012-05-10 16:01:12 +0300

    added my server ip as env var

--- login_general.sh
+++ login_general.sh
@@ -5,5 +5,6 @@ export EDITOR="vim"
 export BROWSER="firefox"
 export CLICOLOR=1
 export AUDIODRIVER="alsa"
+export MYSERV="178.79.141.250"
 
 export PATH="$HOME/.rbenv/bin:$PATH:$HOME/p/my/scripts:$HOME/p/my/fork/ruby-build/bin:$HOME/bin:$HOME/.local/bin"
