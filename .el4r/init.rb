ef9d46881a7137ef0e0b992b9033117ed1d72c74
Ales Guzik <me@aguzik.net>
2014-07-10 23:46:15 +0300

    disable xiki

--- .el4r/init.rb
+++ .el4r/init.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH << "/home/me/p/fork/xiki/lib/"
 # $LOAD_PATH << "/projects/xiki/lib/"
 require 'xiki'
-Xiki.init
-Xiki::KeyShortcuts.keys   # Use default key shortcuts
+# Xiki.init
+# Xiki::KeyShortcuts.keys   # Use default key shortcuts
 # Xiki::Themes.use "Default"  # Use xiki theme
